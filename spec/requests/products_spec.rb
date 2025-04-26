80be03a63e85c407b8e6ccb3235c4ae24a93f952
Kyungsu Kim <roomformilk683@gmail.com>
2025-04-26 01:43:50 +0100

    modify integration test

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -2,23 +2,23 @@ require 'rails_helper'
 
 RSpec.describe "Products API", type: :request do
   it "creates, reads, updates, and deletes a product" do
-    # 1. Create
+    # create
     post "/products", params: { product: { name: "Test Product", description: "Test Description", price: 10.0, available: true } }
     expect(response).to have_http_status(:created)
 
     product = JSON.parse(response.body)
 
-    # 2. Read
+    # read
     get "/products/#{product["id"]}"
     expect(response).to have_http_status(:ok)
     expect(JSON.parse(response.body)["name"]).to eq("Test Product")
 
-    # 3. Update
+    # update
     patch "/products/#{product["id"]}", params: { product: { price: 20.0 } }
     expect(response).to have_http_status(:ok)
     expect(JSON.parse(response.body)["price"].to_f).to eq(20.0)
        
-    # 4. Delete
+    # delete
     delete "/products/#{product["id"]}"
     expect(response).to have_http_status(:no_content)
   end
